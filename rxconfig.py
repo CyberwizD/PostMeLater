1312313aa6b216e7ab3eb79937f5efd76c2b479d
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-05-25 16:48:55 +0100

    feat: enhance configuration with frontend packages and additional plugins

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,11 @@
 import reflex as rx
 
-config = rx.Config(app_name="PostMeLater", plugins=[rx.plugins.TailwindV3Plugin()])
+config = rx.Config(
+    app_name="PostMeLater",
+    frontend_packages=["es-toolkit@1.39.3"],
+    plugins=[
+        rx.plugins.TailwindV3Plugin(),
+        rx.plugins.RadixThemesPlugin(theme=rx.theme(appearance="light")),
+        rx.plugins.SitemapPlugin(),
+    ],
+)
